81765ab86eb0b62a14732d9f481fb21af9cff303
Ilham Ramadhani Saputro <dkijakartaramasaputro@gmail.com>
2022-09-09 18:19:23 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] JoystickScript joystickScript;
+    [SerializeField] JoystickScript rightJoystick;
+
+    public GameObject capsule;
+
+    float walkSpeed = 10f;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float curSpeedX = walkSpeed * Input.GetAxis("Vertical");
+        float curSpeedY = walkSpeed * Input.GetAxis("Horizontal");
+
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (joystickScript.inputDir != Vector2.zero)
+        {
+            moveDirection = (forward * walkSpeed * joystickScript.inputDir.y) + (right * walkSpeed * joystickScript.inputDir.x);
+
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+
+        if (rightJoystick.inputDir != Vector2.zero)
+        {
+            capsule.transform.localEulerAngles = new Vector3(0f, Mathf.Atan2(rightJoystick.inputDir.x, rightJoystick.inputDir.y) * 180 / Mathf.PI, 0f);
+        }
+
+    }
+}
