973f08eb424ca3b6974aaba891063338161cf494
Ilham Ramadhani Saputro <dkijakartaramasaputro@gmail.com>
2022-09-10 15:40:56 +0700

    make main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject controlPanel;
+    public GameObject weaponPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlPanel.SetActive(false);
+        weaponPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Back()
+    {
+        controlPanel.SetActive(false);
+        weaponPanel.SetActive(false);
+    }
+
+    public void OpenControl()
+    {
+        controlPanel.SetActive(true);
+    }
+
+    public void OpenWeapon()
+    {
+        weaponPanel.SetActive(true);
+    }
+
+    public void StartGame(int weap)
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+        Application.Quit();
+    }
+}
